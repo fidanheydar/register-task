f49ac9b1bad739a48b17f8892f52485d6593b711
fidanheydar <125346195+fidanheydar@users.noreply.github.com>
2023-05-11 11:43:59 +0400

    register form is added

--- index.js
+++ index.js
@@ -4,62 +4,47 @@ const confirmPassword = document.getElementById("rePassword");
 const tableBody = document.querySelector("tbody");
 const registerButton = document.querySelector("#register");
 const arr = [];
-const loginInfo = document.querySelector("div");
+const emailLoginInput = document.getElementById("emailCheck");
+const passwordLoginInput = document.getElementById("passCheck");
+const loginBtn = document.getElementById("loginBtn");
+const loginInfo = document.querySelector("div")
 
 
 
-registerButton.addEventListener("click", ()=>{
+registerButton.addEventListener("click", () => {
+
+    if (!emailInput.value.trim() || !passwordInput.value.trim() || !rePassword.value.trim() || passwordInput.value.trim() != confirmPassword.value.trim()) {
+        alert("Duzgun daxil  edin")
+        return
+    }
     const user = {
-        mail:emailInput.value,
+        email: emailInput.value,
         password: passwordInput.value
     }
     arr.push(user)
-    if(!emailInput.value.trim() || !passwordInput.value.trim() || !rePassword.value.trim() || passwordInput.value.trim()!=confirmPassword.value.trim()){
-        alert("Duzgun daxil edin")
-        return
-    }
-    else{
-        const newTrItem = document.createElement("tr");
-        newTrItem.innerHTML=`
-        <td>${emailInput.value}</td>
-        <td>${passwordInput.value}</td>
-        `;
-        tableBody.append(newTrItem);
-
-    }
+    const newTrItem = document.createElement("tr");
+    newTrItem.innerHTML = `
+    <td>${emailInput.value}</td>
+    <td>${passwordInput.value}</td>
+    `;
+    tableBody.append(newTrItem);
     emailInput.value = "";
     passwordInput.value = "";
     rePassword.value = "";
 
 
 })
-const emailLoginInput = document.getElementById("emailCheck");
-const passwordLoginInput = document.getElementById("passCheck");
-const loginBtn = document.getElementById("loginBtn");
 
+loginBtn.addEventListener("click", () => {
 
-
-
-
-loginBtn.addEventListener("click", ()=>{
-    
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i].mail==emailCheck.value && passCheck.value == arr[i].password){
-            const newH3Item = document.createElement("h3");
-            newH3Item.innerHTML=`
-            <h3>Logged in</h3>
-            `
-            loginInfo.append(newH3Item);
+        if (arr[i].email == emailCheck.value && passCheck.value == arr[i].password) {
+            loginInfo.textContent = "Logged in"
             emailCheck.value = '';
             passCheck.value = '';
             return
         }
-        else{
-            const newH3Item = document.createElement("h3");  
-            newH3Item.innerHTML=`
-            <h3> Not logged in</h3>
-            `
-        }
+
     }
-    
+    loginInfo.textContent = "Not logged in"
 })
